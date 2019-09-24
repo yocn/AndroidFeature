501e88e21d5e48120158721ed0b8bbfff705a300
Yocn <zhaoyingkun@163.com>
2019-09-24 18:08:42 +0800

    update

--- app/src/main/java/com/yocn/af/view/splash/MySplashActivity.java
+++ app/src/main/java/com/yocn/af/view/splash/MySplashActivity.java
@@ -24,50 +24,82 @@ import com.yocn.af.view.base.BaseActivity;
  */
 public class MySplashActivity extends BaseActivity {
 
-    String color = "https://i.loli.net/2019/09/24/t1GcrQ4O6TK7efw.jpg";
-    String gray = "https://i.loli.net/2019/09/24/Ny1uEHhmYrKPJoG.jpg";
-    ImageView mImageIV;
-    ImageView mImageIVTop;
+    String color1 = "https://i.loli.net/2019/09/24/t1GcrQ4O6TK7efw.jpg";
+    String gray1 = "https://i.loli.net/2019/09/24/Ny1uEHhmYrKPJoG.jpg";
+    //https://i.loli.net/2019/09/24/lyJ9AziRIE264ux.jpg
+    //https://i.loli.net/2019/09/24/QdEOUKNeaishADR.jpg
+    String color2 = "https://i.loli.net/2019/09/24/lyJ9AziRIE264ux.jpg";
+    String gray2 = "https://i.loli.net/2019/09/24/QdEOUKNeaishADR.jpg";
+    ImageView mImageIV1;
+    ImageView mImageIVTop1;
+    ImageView mImageIV2;
+    ImageView mImageIVTop2;
     Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        mImageIV = findViewById(R.id.iv_image);
-        mImageIVTop = findViewById(R.id.iv_image_top);
+        mImageIV1 = findViewById(R.id.iv_image1);
+        mImageIVTop1 = findViewById(R.id.iv_image_top1);
+        mImageIV2 = findViewById(R.id.iv_image2);
+        mImageIVTop2 = findViewById(R.id.iv_image_top2);
         context = this;
-        Glide.clear(mImageIV);
-        Glide.clear(mImageIVTop);
         Glide.with(context)
-                .load(gray)
+                .load(gray1)
+                .centerCrop()
                 .diskCacheStrategy(DiskCacheStrategy.SOURCE)
                 .into(simpleTarget);
+        Glide.with(context)
+                .load(gray2)
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(simpleTarget2);
 
     }
 
     SimpleTarget<GlideDrawable> simpleTarget = new SimpleTarget<GlideDrawable>() {
         @Override
         public void onResourceReady(GlideDrawable resource, GlideAnimation glideAnimation) {
-            mImageIV.setImageDrawable(resource);
+            mImageIV1.setImageDrawable(resource);
             Glide.with(getApplicationContext())
-                    .load(color)
+                    .load(color1)
+                    .centerCrop()
                     .diskCacheStrategy(DiskCacheStrategy.SOURCE)
                     .animate(animationObject)
-                    .into(mImageIVTop);
+                    .into(mImageIVTop1);
         }
     };
 
-
-    ViewPropertyAnimation.Animator animationObject = new ViewPropertyAnimation.Animator() {
+    SimpleTarget<GlideDrawable> simpleTarget2 = new SimpleTarget<GlideDrawable>() {
         @Override
-        public void animate(View view) {
-            // if it's a custom view class, cast it here
-            // then find subviews and do the animations
-            // here, we just use the entire view for the fade animation
-            ObjectAnimator fadeAnim = ObjectAnimator.ofFloat(view, "alpha", 0f, 1f);
-            fadeAnim.setDuration(2500);
-            fadeAnim.start();
+        public void onResourceReady(GlideDrawable resource, GlideAnimation glideAnimation) {
+            mImageIV2.setImageDrawable(resource);
+            Glide.with(getApplicationContext())
+                    .load(color2)
+                    .centerCrop()
+                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                    .animate(animationObject)
+                    .into(mImageIVTop2);
         }
     };
+
+
+    ViewPropertyAnimation.Animator animationObject = view -> {
+        // if it's a custom view class, cast it here
+        // then find subviews and do the animations
+        // here, we just use the entire view for the fade animation
+        ObjectAnimator fadeAnim = ObjectAnimator.ofFloat(view, "alpha", 0f, 1f);
+        fadeAnim.setDuration(2500);
+        fadeAnim.start();
+    };
+
+    @Override
+    protected void onDestroy() {
+        mImageIV1 = null;
+        mImageIV2 = null;
+        mImageIVTop1 = null;
+        mImageIVTop2 = null;
+        super.onDestroy();
+    }
 }
