84dc330dab32b3f1ee27e6bac9904ca6146d63af
yocn <zhaoyingkun@163.com>
2021-04-14 19:44:21 +0800

    增加文字变化动画

--- app/src/main/java/com/yocn/af/view/widget/WeChatVoiceView.java
+++ app/src/main/java/com/yocn/af/view/widget/WeChatVoiceView.java
@@ -5,7 +5,6 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
-import android.graphics.Rect;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.AttributeSet;
@@ -24,11 +23,11 @@ import androidx.annotation.Nullable;
 
 public class WeChatVoiceView extends FrameLayout {
     private String TAG = "WeChatVoiceView";
-    private TextView voice;
     private WeChatVoiceBottomArc weChatVoiceBottomArcLight;
     private WeChatVoiceBottomArc weChatVoiceBottomArcDark;
     private final int ANIM_DURATION = 300;
     private final int ANIM_DURATION_TEXT = 500;
+    private final int ANIM_DURATION_TEXT_BIGGER = 100;
     private ImageView voiceIv;
     private int bottomArcTransY;
     private TextView voiceTv;
@@ -41,6 +40,13 @@ public class WeChatVoiceView extends FrameLayout {
     boolean darkAniming = false;
     private AnimatorSet bottomArcSet;
     private AnimatorSet textAnimSet;
+    private int[] screenWH;
+    private AnimatorSet cancelTvScaleBiggerAnim;
+    private AnimatorSet translateTvScaleBiggerAnim;
+    private AnimatorSet cancelTvScaleSmallAnim;
+    private AnimatorSet translateTvScaleSmallAnim;
+    private TextView cancelHintTv;
+    private TextView translateHintTv;
 
     public WeChatVoiceView(@NonNull Context context) {
         this(context, null);
@@ -62,7 +68,6 @@ public class WeChatVoiceView extends FrameLayout {
     }
 
     private void initView() {
-        voice = findViewById(R.id.voice);
         voiceIv = findViewById(R.id.iv_voice);
         weChatVoiceBottomArcLight = findViewById(R.id.bottom_arc_light);
         weChatVoiceBottomArcDark = findViewById(R.id.bottom_arc_dark);
@@ -70,6 +75,9 @@ public class WeChatVoiceView extends FrameLayout {
         translateTv = findViewById(R.id.tv_trans);
         bottomArcTransY = getResources().getDimensionPixelOffset(R.dimen.arc_height_light);
         voiceTv = findViewById(R.id.voice);
+        cancelHintTv = findViewById(R.id.tv_cancel_text);
+        translateHintTv = findViewById(R.id.tv_trans_text);
+        screenWH = DisplayUtil.getHW(getContext());
     }
 
     private void initAnimation() {
@@ -102,6 +110,7 @@ public class WeChatVoiceView extends FrameLayout {
             public void onAnimationEnd(Animator animation, boolean isReverse) {
                 darkAniming = false;
                 currentArcLight = false;
+                voiceIv.setImageResource(R.drawable.ic_voice_dark);
             }
         });
 
@@ -112,6 +121,60 @@ public class WeChatVoiceView extends FrameLayout {
             public void onAnimationEnd(Animator animation, boolean isReverse) {
                 lightAniming = false;
                 currentArcLight = true;
+                voiceIv.setImageResource(R.drawable.ic_voice);
+            }
+        });
+
+        float src = 1f, tar = 1.2f;
+        ObjectAnimator cancelTvScaleXBiggerAnim = ObjectAnimator.ofFloat(cancelTv, "scaleX", src, tar);
+        ObjectAnimator cancelTvScaleYBiggerAnim = ObjectAnimator.ofFloat(cancelTv, "scaleY", src, tar);
+        ObjectAnimator cancelTvScaleXSmallAnim = ObjectAnimator.ofFloat(cancelTv, "scaleX", tar, src);
+        ObjectAnimator cancelTvScaleYSmallAnim = ObjectAnimator.ofFloat(cancelTv, "scaleY", tar, src);
+
+        cancelTvScaleBiggerAnim = new AnimatorSet();
+        cancelTvScaleBiggerAnim.playTogether(cancelTvScaleXBiggerAnim, cancelTvScaleYBiggerAnim);
+        cancelTvScaleBiggerAnim.setDuration(ANIM_DURATION_TEXT_BIGGER);
+        cancelTvScaleBiggerAnim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation, boolean isReverse) {
+                cancelTvAnimationing = false;
+                cancelTvBig = true;
+            }
+        });
+        cancelTvScaleSmallAnim = new AnimatorSet();
+        cancelTvScaleSmallAnim.playTogether(cancelTvScaleXSmallAnim, cancelTvScaleYSmallAnim);
+        cancelTvScaleSmallAnim.setDuration(ANIM_DURATION_TEXT_BIGGER);
+        cancelTvScaleSmallAnim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation, boolean isReverse) {
+                cancelTvAnimationing = false;
+                cancelTvBig = false;
+            }
+        });
+
+        ObjectAnimator translateTvScaleXBiggerAnim = ObjectAnimator.ofFloat(translateTv, "scaleX", src, tar);
+        ObjectAnimator translateTvScaleYBiggerAnim = ObjectAnimator.ofFloat(translateTv, "scaleY", src, tar);
+        ObjectAnimator translateTvScaleXSmallAnim = ObjectAnimator.ofFloat(translateTv, "scaleX", tar, src);
+        ObjectAnimator translateTvScaleYSmallAnim = ObjectAnimator.ofFloat(translateTv, "scaleY", tar, src);
+
+        translateTvScaleBiggerAnim = new AnimatorSet();
+        translateTvScaleBiggerAnim.playTogether(translateTvScaleXBiggerAnim, translateTvScaleYBiggerAnim);
+        translateTvScaleBiggerAnim.setDuration(ANIM_DURATION_TEXT_BIGGER);
+        translateTvScaleBiggerAnim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation, boolean isReverse) {
+                translateTvAnimationing = false;
+                translateTvBig = true;
+            }
+        });
+        translateTvScaleSmallAnim = new AnimatorSet();
+        translateTvScaleSmallAnim.playTogether(translateTvScaleXSmallAnim, translateTvScaleYSmallAnim);
+        translateTvScaleSmallAnim.setDuration(ANIM_DURATION_TEXT_BIGGER);
+        translateTvScaleSmallAnim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation, boolean isReverse) {
+                translateTvAnimationing = false;
+                translateTvBig = false;
             }
         });
     }
@@ -152,10 +215,77 @@ public class WeChatVoiceView extends FrameLayout {
         float y = ev.getY();
         if (weChatVoiceBottomArcLight.isOnRect(x, y)) {
             tryChangeToLight();
+            tryChangeCancelTextToSmall();
+            tryChangeTranslateTextToSmall();
+            voiceTv.setVisibility(View.VISIBLE);
         } else {
+            // 不在区域里，看在屏幕左边右边
             tryChangeToDark();
+            if (x < screenWH[0] / 2) {
+                tryChangeCancelTextToBigger();
+                tryChangeTranslateTextToSmall();
+            } else {
+                tryChangeTranslateTextToBigger();
+                tryChangeCancelTextToSmall();
+            }
+            voiceTv.setVisibility(View.GONE);
         }
         return true;
+    }
+
+    private void setTextBigger(TextView text) {
+        text.setTextColor(getResources().getColor(R.color.black));
+        text.setBackgroundResource(R.drawable.bg_trans_oval_white);
+    }
+
+    private void setTextSmall(TextView text) {
+        text.setTextColor(getResources().getColor(R.color.write));
+        text.setBackgroundResource(R.drawable.bg_trans_oval);
+    }
+
+    boolean cancelTvBig = false;
+    boolean translateTvBig = false;
+    boolean cancelTvAnimationing = false;
+    boolean translateTvAnimationing = false;
+
+    private void tryChangeCancelTextToBigger() {
+        if (cancelTvBig || cancelTvAnimationing) {
+            return;
+        }
+        cancelTvAnimationing = true;
+        setTextBigger(cancelTv);
+        cancelHintTv.setVisibility(View.VISIBLE);
+        cancelTvScaleBiggerAnim.start();
+    }
+
+    private void tryChangeCancelTextToSmall() {
+        if (!cancelTvBig || cancelTvAnimationing) {
+            return;
+        }
+        cancelHintTv.setVisibility(View.GONE);
+        cancelTvAnimationing = true;
+        setTextSmall(cancelTv);
+        cancelTvScaleSmallAnim.start();
+    }
+
+    private void tryChangeTranslateTextToBigger() {
+        if (translateTvBig || translateTvAnimationing) {
+            return;
+        }
+        translateHintTv.setVisibility(View.VISIBLE);
+        translateTvAnimationing = true;
+        setTextBigger(translateTv);
+        translateTvScaleBiggerAnim.start();
+    }
+
+    private void tryChangeTranslateTextToSmall() {
+        if (!translateTvBig || translateTvAnimationing) {
+            return;
+        }
+        translateHintTv.setVisibility(View.GONE);
+        translateTvAnimationing = true;
+        setTextSmall(translateTv);
+        translateTvScaleSmallAnim.start();
     }
 
     private void tryChangeToDark() {
